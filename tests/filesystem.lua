adb017c476597ac9269852fd520dc0db656057c9
TurtleP <jpostelnek@outlook.com>
2024-03-20 08:37:17 -0400

    test to make sure loading b or t errors below 5.2 and no JIT

--- tests/filesystem.lua
+++ tests/filesystem.lua
@@ -331,11 +331,18 @@ love.test.filesystem.load = function(test)
     test:assertEquals(nil, errormsg2, 'check no error message')
     test:assertEquals(1, chunk2(), 'check lua file runs')
   else
-    -- check valid lua file (any load)
-    local chunk4, errormsg4 = love.filesystem.load('test1.lua', 'bt')
-    test:assertEquals(nil, errormsg4, 'check no error message')
-    test:assertEquals(1, chunk4(), 'check lua file runs')
+    local _, errormsg3 = love.filesystem.load('test1.lua', 'b')
+    test:assertNotEquals(nil, errormsg3, 'check for an error message')
+
+    local _, errormsg4 = love.filesystem.load('test1.lua', 't')
+    test:assertNotEquals(nil, errormsg4, 'check for an error message')
   end
+
+  -- check valid lua file (any load)
+  local chunk5, errormsg5 = love.filesystem.load('test1.lua', 'bt')
+  test:assertEquals(nil, errormsg5, 'check no error message')
+  test:assertEquals(1, chunk5(), 'check lua file runs')
+
   -- check invalid lua file
   local ok, chunk, err = pcall(love.filesystem.load, 'test2.lua')
   test:assertFalse(ok, 'check invalid lua file')
